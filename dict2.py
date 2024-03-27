fc018c14fc9504fd895164db0ef2327b2b78e7e1
manikantareddy2801@gmail.com <manikantareddy2801@.com>
2024-03-27 11:46:46 +0530

    I added some files

--- dict2.py
+++ dict2.py
@@ -1,3 +1,8 @@
-dict={"1:mani","2:raju","3:vijju","4:deepu"}
-dict.update({"5:colour"})
-print(dict.update())
+dict={"1":"mani","2":"vijay","3":"nani","4":"raju"}
+print(dict)
+key= dict.keys()
+print(key)
+value=dict.values()
+print(value)
+dict["5"]="suresh"
+print("update_dict:",dict)
